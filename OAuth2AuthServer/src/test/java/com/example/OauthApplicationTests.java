c8e56c5e1d3490bd816a59198371ed1c658f0704
michael <michael@DESKTOP-468O5LS>
2018-07-25 11:13:59 +0800

    upgrade to spring boot 1.5.7

--- OAuth2AuthServer/src/test/java/com/example/OauthApplicationTests.java
+++ OAuth2AuthServer/src/test/java/com/example/OauthApplicationTests.java
@@ -2,15 +2,16 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = AuthServerApplication.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AuthServerApplication.class)
 public class OauthApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		System.out.println("context loads ...");
 	}
 
 }
